b5d3bea366553fda56b1afc7e6b2ee852ae9106f
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 17:48:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,15 @@
 def my_collect(array)
-  i = 0 
-  collection = []
+  if array.length == 0 
+    return "Hey, this won't work."
+  else
+    i = 0 
+    collection = []
   
-  while i < array.length
-    
+    while i < array.length
+    collection << yield(array[i])
+    i += 1
+    end
   end
+  collection
 end
 
